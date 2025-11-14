516384c0ff66e81773ba5d1b7ea92a586e999c3d
iym1511 <iym1511@naver.com>
2025-11-14 16:26:43 +0900

    [test] test: ci/cd 테스트 커밋4

--- my-app/src/fetchData/fetch-get.ts
+++ my-app/src/fetchData/fetch-get.ts
@@ -1,8 +1,16 @@
 import {createKy} from "@/util/api";
 
+interface UserType {
+    created_at : string;
+    email : string;
+    id: string;
+    name : string;
+    password_hash : string;
+}
+
 export const apiTest = async () => {
     try {
-        const data = await createKy().get("get-users").json<any>();
+        const data = await createKy().get("get-users").json<UserType>();
 
         console.log("✅ API 결과:", data);
         return data;
