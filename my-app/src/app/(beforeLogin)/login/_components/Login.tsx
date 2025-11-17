788becdf2099bb4805b3d3ba2dcdf8254029a248
iym1511 <iym1511@naver.com>
2025-11-17 12:00:04 +0900

    :recycle: [refactor] 로그인 url 환경변수로 변경

--- my-app/src/app/(beforeLogin)/login/_components/Login.tsx
+++ my-app/src/app/(beforeLogin)/login/_components/Login.tsx
@@ -43,7 +43,7 @@ function Login() {
     /* 회원가입 함수 */
     const handleSignUp = async () => {
         try {
-            const res = await fetch('/ptc/signup', {
+            const res = await fetch(`/${process.env.NEXT_PUBLIC_API_URL}/signup`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
