9bda6cc49b81362f1fb83c71308f3498cc73540d
iym1511 <iym1511@naver.com>
2025-11-17 17:05:30 +0900

    :recycle: [refactor] 업데이트23456

--- my-node/src/controllers/authController.ts
+++ my-node/src/controllers/authController.ts
@@ -182,7 +182,7 @@ export const login = async (req: Request, res: Response) => {
         if (error || !user) {
             return res.status(401).json({
                 error: "INVALID_CREDENTIALS",
-                message: `이메일 또는 비밀번호가 올바르지 않습니다 ${email}, ${password}.`,
+                message: `이메일 또는 비밀번호가 올바르지 않습니다 ${data}, ${password}.`,
             });
         }
 
