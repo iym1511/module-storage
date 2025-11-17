e7b10b84d912e5c0697444d7264e5a7bef95a227
iym1511 <iym1511@naver.com>
2025-11-17 16:45:19 +0900

    :recycle: [refactor] 업데이트234

--- my-node/src/server.ts
+++ my-node/src/server.ts
@@ -1,11 +1,7 @@
 import app from "./app";
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-// ✅ 0.0.0.0 으로 변경 (모든 네트워크 인터페이스에서 접속 허용)
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`서버가 http://0.0.0.0:${PORT} 에서 실행 중입니다.`);
+app.listen(8000, '0.0.0.0', () => {
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
-// app.listen(PORT ,() => {
-//     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
-// });
