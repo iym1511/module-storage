70d6fc309a29de4bf9a1c32b155a9446e732d819
iym1511 <iym1511@naver.com>
2025-11-18 16:03:48 +0900

    :recycle: [refactor] next route url 수정4

--- my-node/src/server.ts
+++ my-node/src/server.ts
@@ -2,6 +2,6 @@ import app from "./app";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(8000, '0.0.0.0', () => {
+app.listen(8000,  () => {
     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
