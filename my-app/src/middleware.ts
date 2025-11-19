5e4349b5a836d94073d5b5215bc0afc8a17fbd2c
iym1511 <iym1511@naver.com>
2025-11-19 10:21:21 +0900

    :recycle: [refactor] 미들웨어 수정

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -48,7 +48,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: [
-        "/((?!api|_next/static|_next/image|favicon.ico).*)"
-    ]
+    matcher: ["/", "/login", "/home",]
 };
